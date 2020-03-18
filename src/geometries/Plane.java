cc00404c4540501b578cf2fb696eb532113b849d
reouven smadja <rusmadja@g.jct.ac.il>
2020-03-18 12:41:35 +0200

    test

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -8,7 +8,9 @@ public class Plane implements Geometry {
     Vector _normal;
 
     @Override
-    public Vector getNormal(Point3D other) {
+    public Vector getNormal(Point3D other)
+    {
+        int ok=0;
         return null;
     }
 }
