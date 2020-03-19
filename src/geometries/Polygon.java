3255be6cd4258e9b5deb1f451f4689c859e5961e
reouven smadja <rusmadja@g.jct.ac.il>
2020-03-19 23:59:32 +0200

    all ok enfin presque

--- src/geometries/Polygon.java
+++ src/geometries/Polygon.java
@@ -38,7 +38,8 @@ public class Polygon implements Geometry {
      *                                  <li>The polygon is concave (not convex></li>
      *                                  </ul>
      */
-    public Polygon(Point3D... vertices) {
+    public Polygon(Point3D... vertices)
+    {
         if (vertices.length < 3)
             throw new IllegalArgumentException("A polygon can't have less than 3 vertices");
 //========>>>>>>>>               _vertices = List.of(vertices);
